bd64e722efb1fc080c80d2d244b1cc1383b3d98e
Gülşah Alıcı <gulsah.alici1@gmail.com>
2022-09-22 13:09:53 +0300

    import vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+
+import Vue from 'vue'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+import App from './components/App'
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+})
+
---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+     <b-alert show>Hello world!</b-alert>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
